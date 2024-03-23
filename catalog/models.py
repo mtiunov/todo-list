dc13614b1e4bec1f7e91b7abb587398e674c7869
Mikhail <tiunovmixs@gmail.com>
2024-03-23 18:07:42 +0200

    Create models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    content = models.TextField()
+    published_date = models.DateTimeField(default=timezone.now)
+    deadline = models.DateTimeField(verbose_name="deadline", blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+
+    class Meta:
+        ordering = ["deadline", "-published_date"]
+
+    def __str__(self):
+        return f"{self.content} ({self.published_date} {self.tags})"
