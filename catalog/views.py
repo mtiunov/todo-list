70189889540796df5839c6d5492312285a45cf40
Mikhail <tiunovmixs@gmail.com>
2024-03-23 21:36:52 +0200

    Creat base.html, index.html, add superuser, update settings

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Task, Tag
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+    num_tags = Tag.objects.count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_tags": num_tags,
+    }
+
+    return render(request, "catalog/index.html", context=context)
