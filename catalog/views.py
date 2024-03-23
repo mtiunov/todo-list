3c2350135b8f938b2cdca5110c81dd6a9dd7b33f
Mikhail <tiunovmixs@gmail.com>
2024-03-24 00:01:59 +0200

    Add tasks list to the homepage index.html

--- catalog/views.py
+++ catalog/views.py
@@ -4,11 +4,12 @@ from .models import Task, Tag
 
 def index(request):
     """View function for the home page of the site."""
-
+    tasks = Task.objects.all()
     num_tasks = Task.objects.count()
     num_tags = Tag.objects.count()
 
     context = {
+        "tasks": tasks,
         "num_tasks": num_tasks,
         "num_tags": num_tags,
     }
