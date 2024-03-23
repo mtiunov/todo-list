bbecde36eacdc450258dc7d005d88805a89bb627
Mikhail <tiunovmixs@gmail.com>
2024-03-23 22:49:22 +0200

    Add superuser, tasks, tags & customizing admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from catalog.models import Task, Tag
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "published_date", "deadline", ]
+    search_fields = ["content", ]
+
+
+admin.site.register(Tag)
